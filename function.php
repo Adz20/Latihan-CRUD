9521df4be6487231ba235fe6a4614a0be2127664
Adz20 <86099827+Adz20@users.noreply.github.com>
2021-06-21 08:45:06 +0700

    Function.php

--- function.php
+++ function.php
@@ -37,4 +37,25 @@ function hapus($id) {
     return mysqli_affected_rows($conn);
 }
 
+function ubah($data) {
+    global $conn;
+
+    $nis = htmlspecialchars($data["nis"]);
+    $nama = htmlspecialchars($data["nama"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+
+    $query = "UPDATE datasiswa SET
+                nis ='$nis',
+                nama ='$nama',
+                email = '$email',
+                jurusan = '$jurusan',
+                gambar = '$gambar'
+              WHERE id = $id  
+              
+            ";
+    mysqli_query($conn, $query);
+    return mysqli_affected_rows($conn);
+}
 ?>
